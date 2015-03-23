b5971b6fc83da77c02864231be974fdf653ee76a
acreilly <acreilly3@gmail.com>
2015-03-23 17:23:46 -0400

    added javascript, fixed route

--- app/assets/javascripts/spree/backend/spree_orders_csv_export.js
+++ app/assets/javascripts/spree/backend/spree_orders_csv_export.js
@@ -1,2 +1,15 @@
 // Placeholder manifest file.
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/backend/all.js'
+$(document).ready(function() {
+
+  $("#all_orders").on('click', function(e) {
+    if (this.checked){
+      $(".order_row").prop('checked', true);
+      $("#list_state_change").css('visibility', 'inherit');
+    }else{
+      $(".order_row").prop('checked', false);
+      $("#list_state_change").css('visibility', 'hidden');
+    }
+  });
+
+});
