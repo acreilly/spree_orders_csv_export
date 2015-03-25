79956dd20c1d5f139f29b97d5d67f528598bdaef
acreilly <acreilly3@gmail.com>
2015-03-25 13:18:53 -0400

    path set up for ns

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Spree::Core::Engine.routes.draw do
-  post '/admin/orders_list_actions' => 'orders#orders_list_actions'
+  post '/shop/admin/orders_list_actions' => 'orders#orders_list_actions'
 end
