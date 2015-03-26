0f7155dab5b56f42d0e63e6f5f33a2c68872ddc3
kinduff <alejandro@abarcarodriguez.com>
2015-03-26 12:40:45 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Spree::Core::Engine.routes.draw do
-  post '/admin/orders_list_actions' => 'orders#orders_list_actions'
+  post '/admin/orders_list_actions' => 'orders#orders_list_actions', as: :orders_list_actions
 end
